14ac6bb8b06d8f11a5cfd7ffcf02cd34900b8e3e
djek-sweng <djek@gmx.ch>
2023-03-19 21:16:30 +0100

    CommandLineOptions

--- src/Wizards.SolutionGenerator.Executable/Options/CommandLineOptions.cs
+++ src/Wizards.SolutionGenerator.Executable/Options/CommandLineOptions.cs
@@ -2,18 +2,33 @@ namespace Wizards.SolutionGenerator.Executable.Options;
 
 public class CommandLineOptions
 {
-    [Option("generate-makefile", Required = false, HelpText = "GenerateMakefile")]
+    [Option(
+        "generate-makefile",
+        Required = false,
+        HelpText = "Generate makefile.")]
     public bool GenerateMakefile { get; set; } = false;
 
-    [Option("generate-solution-from-directory", Required = false, HelpText = "GenerateSolutionFromDirectory")]
+    [Option(
+        "generate-solution-from-directory",
+        Required = false,
+        HelpText = "Generate solution from directory.")]
     public bool GenerateSolutionFromDirectory { get; set; } = false;
 
-    [Option("generate-solution-from-makefile", Required = false, HelpText = "GenerateSolutionFromMakefile")]
+    [Option(
+        "generate-solution-from-makefile",
+        Required = false,
+        HelpText = "Generate solution from makefile.")]
     public bool GenerateSolutionFromMakefile { get; set; } = false;
 
-    [Option("source", Required = false, HelpText = "Source")]
+    [Option(
+        "source",
+        Required = true,
+        HelpText = "Source directory or file.")]
     public string Source { get; set; } = string.Empty;
 
-    [Option("name", Required = false, HelpText = "Name")]
+    [Option(
+        "name",
+        Required = true,
+        HelpText = "Name of generated file.")]
     public string Name { get; set; } = string.Empty;
 }
