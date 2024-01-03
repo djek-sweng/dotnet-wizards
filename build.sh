8edfe115903bad83f86ed0c91a90e98aa9bfc628
David <78724080+djek-sweng@users.noreply.github.com>
2024-01-03 17:41:29 +0100

    Update SDK .NET8 (#4)

--- build.sh
+++ build.sh
@@ -7,16 +7,16 @@ dotnet clean src/Wizards.sln
 dotnet restore src/Wizards.sln
 
 dotnet build src/Wizards.sln \
-    --no-restore \
-    --configuration Release
+  --no-restore \
+  --configuration Release
 
 dotnet test src/Wizards.sln \
-    --no-restore \
-    --no-build \
-    --verbosity normal
+  --no-restore \
+  --no-build \
+  --verbosity normal
 
 dotnet publish src/Wizards.SolutionGenerator.Executable/Wizards.SolutionGenerator.Executable.csproj \
-    --no-restore \
-    --no-build \
-    --configuration Release \
-    --output dist/dotnet-solution-generator
+  --no-restore \
+  --no-build \
+  --configuration Release \
+  --output dist/dotnet-solution-generator
